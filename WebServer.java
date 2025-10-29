92f8662eb2cc646f5f7e996a7604da05d5b4ea76
Hadi Abou Hawili <aboualhood2005@hotmail.com>
2025-10-29 20:02:24 +0200

    first commit

---
+++ WebServer.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class WebServer
+{
+public static void main(String[] args) throws Exception
+{
+//. . .
+}
+}
+class HttpRequest implements Runnable
+{
+    @Override
+    public void run() {
+        // Your code goes here — what should happen when the thread starts
+        System.out.println("Handling HTTP request...");
+    }
+}
